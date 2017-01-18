432ffd4f3b3428b850a19c1138cda9ba1831821c
lateminer <late-miner@ya.ru>
2017-01-18 21:47:11 +0300

    Remove rescanning after importing private key

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -64,8 +64,13 @@ Value importprivkey(const Array& params, bool fHelp)
         if (!pwalletMain->AddKey(key))
             throw JSONRPCError(RPC_WALLET_ERROR, "Error adding key to wallet");
 
-        pwalletMain->ScanForWalletTransactions(pindexGenesisBlock, true);
-        pwalletMain->ReacceptWalletTransactions();
+        // Whether to perform rescan after import
+        bool fRescan = false;
+        
+        if (fRescan) {
+            pwalletMain->ScanForWalletTransactions(pindexGenesisBlock, true);
+            pwalletMain->ReacceptWalletTransactions();
+        }
     }
 
     return Value::null;
